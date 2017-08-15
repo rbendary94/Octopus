1057650479a2b86aac5c1044fe3584d7ca8170b1
Rana <Rana@Ranas-MacBook-Air.local>
2017-08-15 13:09:01 +0200

    Initial Commit

--- Octopus/Octopus/ViewController.swift
+++ Octopus/Octopus/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
+        //Hi Nesro
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
